e47064e54a0d1cbc76670ac6228e4c4b0183c635
GerwinP <gerwinputtenstein@gmail.com>
2014-02-13 10:34:37 +0100

    BetterTransferProtocol added

---
+++ rdt/src/protocol/BetterTransferProtocol.java
@@ -0,0 +1,31 @@
+package protocol;
+
+import client.INetworkLayerAPI;
+
+public class BetterTransferProtocol implements IDataTransferProtocol{
+
+	@Override
+	public void TimeoutElapsed(Object tag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SetNetworkLayerAPI(INetworkLayerAPI networkLayer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Initialize(TransferMode transferMode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean Tick() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
