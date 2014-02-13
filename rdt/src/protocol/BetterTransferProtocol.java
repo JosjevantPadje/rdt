ffc750f56dbd0cc8151f54847bdbebc6fd157a44
Josje1993 <josje-vt-padje@hotmail.com>
2014-02-13 13:28:58 +0100

    Revert "Added something works"
    This reverts commit 6d3d582f8832b60fc16c1a33bb42398850fa2b7d.

--- rdt/src/protocol/BetterTransferProtocol.java
+++ rdt/src/protocol/BetterTransferProtocol.java
@@ -84,10 +84,8 @@ public class BetterTransferProtocol implements IDataTransferProtocol {
 				if (packetno == 0 || receivedPacket != null) {
 					// We read some bytes, send the packet
 					packetno++; 
-					System.out.println("Something works...");
 					if (networkLayer.Transmit(new Packet(readData)) == TransmissionResult.Failure) {
 						System.out.println("Failure transmitting");
-						
 						return true;
 					}
 				}
